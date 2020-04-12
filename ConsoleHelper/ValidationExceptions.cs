a5ace92d98d2c5ae7742ca2eaff81d2ffc2716a7
AgusBovalina <AgusBovalina@users.noreply.github.com>
2020-04-12 19:24:42 -0300

    1

--- ConsoleHelper/ValidationExceptions.cs
+++ ConsoleHelper/ValidationExceptions.cs
@@ -9,17 +9,17 @@ namespace ConsoleHelper
     public class NullOrEmptyStringException : Exception
     {
         
-        public NullOrEmptyStringException()
+        public NullOrEmptyStringException() : base ("El valor ingresado no puede ser nulo o estar vacío.")
         {
-            Console.WriteLine("El valor ingresado no puede ser nulo o estar vacío.");
+
         }
     }
 
     public class StringTooLongToCharException : Exception
     {
-        public StringTooLongToCharException()
+        public StringTooLongToCharException() : base ("El valor ingresado debe poseer solo un caracter")
         {
-            Console.WriteLine("El valor ingresado debe poseer solo un caracter");
+            
         }
     }
     public class OutOfRangeException : Exception
@@ -27,19 +27,19 @@ namespace ConsoleHelper
         
         string msj = "El valor ingresado no se encuentra dentro del rango requerido.\nDebe ingresar un valor entre";
 
-        public OutOfRangeException(int min, int max)
+        public OutOfRangeException(int min, int max) : base (msj + min + " y " + max)
         {
-            Console.WriteLine(msj + min + " y " + max);
+            
         }
 
-        public OutOfRangeException(double min, double max)
+        public OutOfRangeException(double min, double max) : base(msj + min + " y " + max)
         {
-            Console.WriteLine(msj + min + " y " + max);
+            
         }
 
-        public OutOfRangeException(long min, long max)
+        public OutOfRangeException(long min, long max) : base(msj + min + " y " + max)
         {
-            Console.WriteLine(msj + min + " y " + max);
+            
         }
 
     }
