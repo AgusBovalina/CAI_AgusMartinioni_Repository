59bc8c023d24cb548d7bec54760398dd79054000
AgusBovalina <AgusBovalina@users.noreply.github.com>
2020-05-26 11:01:20 -0300

    1

---
+++ ExpededoraForms/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ExpededoraForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncender = new System.Windows.Forms.Button();
+            this.btnIngresarLata = new System.Windows.Forms.Button();
+            this.btnExtraerLata = new System.Windows.Forms.Button();
+            this.btnMostrarBalance = new System.Windows.Forms.Button();
+            this.btnMostrarStock = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEncender
+            // 
+            this.btnEncender.Location = new System.Drawing.Point(145, 33);
+            this.btnEncender.Name = "btnEncender";
+            this.btnEncender.Size = new System.Drawing.Size(442, 36);
+            this.btnEncender.TabIndex = 0;
+            this.btnEncender.Text = "Encender";
+            this.btnEncender.UseVisualStyleBackColor = true;
+            // 
+            // btnIngresarLata
+            // 
+            this.btnIngresarLata.Location = new System.Drawing.Point(145, 85);
+            this.btnIngresarLata.Name = "btnIngresarLata";
+            this.btnIngresarLata.Size = new System.Drawing.Size(442, 36);
+            this.btnIngresarLata.TabIndex = 3;
+            this.btnIngresarLata.Text = "button3";
+            this.btnIngresarLata.UseVisualStyleBackColor = true;
+            // 
+            // btnExtraerLata
+            // 
+            this.btnExtraerLata.Location = new System.Drawing.Point(145, 139);
+            this.btnExtraerLata.Name = "btnExtraerLata";
+            this.btnExtraerLata.Size = new System.Drawing.Size(442, 36);
+            this.btnExtraerLata.TabIndex = 2;
+            this.btnExtraerLata.Text = "button4";
+            this.btnExtraerLata.UseVisualStyleBackColor = true;
+            // 
+            // btnMostrarBalance
+            // 
+            this.btnMostrarBalance.Location = new System.Drawing.Point(145, 193);
+            this.btnMostrarBalance.Name = "btnMostrarBalance";
+            this.btnMostrarBalance.Size = new System.Drawing.Size(442, 36);
+            this.btnMostrarBalance.TabIndex = 5;
+            this.btnMostrarBalance.Text = "button5";
+            this.btnMostrarBalance.UseVisualStyleBackColor = true;
+            // 
+            // btnMostrarStock
+            // 
+            this.btnMostrarStock.Location = new System.Drawing.Point(145, 244);
+            this.btnMostrarStock.Name = "btnMostrarStock";
+            this.btnMostrarStock.Size = new System.Drawing.Size(442, 36);
+            this.btnMostrarStock.TabIndex = 4;
+            this.btnMostrarStock.Text = "button6";
+            this.btnMostrarStock.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(145, 298);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(442, 36);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "button7";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 366);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnMostrarBalance);
+            this.Controls.Add(this.btnMostrarStock);
+            this.Controls.Add(this.btnIngresarLata);
+            this.Controls.Add(this.btnExtraerLata);
+            this.Controls.Add(this.btnEncender);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncender;
+        private System.Windows.Forms.Button btnIngresarLata;
+        private System.Windows.Forms.Button btnExtraerLata;
+        private System.Windows.Forms.Button btnMostrarBalance;
+        private System.Windows.Forms.Button btnMostrarStock;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
